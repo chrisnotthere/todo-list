bc0df1d9b0b0365b142ede7fac991ce6da0bd332
Chris <mr.chrisnash@gmail.com>
2021-08-05 11:39:28 -0700

    working on UI

--- src/UI.js
+++ src/UI.js
@@ -55,67 +55,41 @@ function createNav(){
     thisWeek.addEventListener('click', () => alert('thisWeek'));
 
 
-    controls.appendChild(inbox);
-    controls.appendChild(today);
-    controls.appendChild(thisWeek);
 
-
-    //// NAV projects 
-    const projects = document.createElement('ul'); 
-    projects.id = 'projects';
+    // projects title
     const projectTitle = document.createElement('li');
     projectTitle.innerText = 'Projects';
     projectTitle.id = 'projectTitle';
     projectTitle.classList.add('noHover');
 
-    // project 1
-    const project1 = document.createElement('li');
-    project1.id = 'project1';
-    const project1Div = document.createElement('div');
-    project1Div.id = 'project1';
-    const project1Icon = document.createElement('i');
-    project1Icon.classList.add('fas', 'fa-tasks');
-    project1Icon.id = 'project1Icon';
-    const project1Title = document.createElement('div');
-    project1Title.id = 'project1Title';
-    project1Title.innerText = 'Project 1';
-    const project1Delete = document.createElement('i');
-    project1Delete.classList.add('far', 'fa-times-circle');
-    project1Delete.id = 'project1Delete';
-
-    project1.appendChild(project1Icon);
-    project1.appendChild(project1Title);
-    project1.appendChild(project1Delete);
-
-    project1Title.addEventListener('click', () => alert('project 1 ...'));
-
-
-    // project 2
-    const project2 = document.createElement('li');
-    project2.id = 'project2';
-    const project2Div = document.createElement('div');
-    project2Div.id = 'project2';
-    const project2Icon = document.createElement('i');
-    project2Icon.classList.add('fas', 'fa-tasks');
-    project2Icon.id = 'project2Icon';
-    const project2Title = document.createElement('div');
-    project2Title.id = 'project2Title';
-    project2Title.innerText = 'Project 2';
-    const project2Delete = document.createElement('i');
-    project2Delete.classList.add('far', 'fa-times-circle');
-    project2Delete.id = 'project2Delete';
-
-    project2.appendChild(project2Icon);
-    project2.appendChild(project2Title);
-    project2.appendChild(project2Delete);
-
-    project2Title.addEventListener('click', () => alert('project 2 ...'));
+    controls.appendChild(inbox);
+    controls.appendChild(today);
+    controls.appendChild(thisWeek);
+    controls.appendChild(projectTitle);
 
 
 
-    projects.appendChild(projectTitle);
-    projects.appendChild(project1);
-    projects.appendChild(project2);
+
+    //// NAV projects 
+    const projects = document.createElement('ul'); 
+    projects.id = 'projects';
+
+    // const projectTitle = document.createElement('li');
+    // projectTitle.innerText = 'Projects';
+    // projectTitle.id = 'projectTitle';
+    // projectTitle.classList.add('noHover');
+
+
+    let projectCounter = 0;
+    // projects.appendChild(projectTitle);
+    projects.appendChild(createProject('Project 1'));
+    projects.appendChild(createProject('school project'));
+    projects.appendChild(createProject('job search'));
+    projects.appendChild(createProject('study for test'));
+
+    const projectList = document.querySelector('#projects')
+    
+    
 
     //add new project button
     const addProjectLogo = document.createElement('i');
@@ -137,8 +111,49 @@ function createNav(){
 
 
     nav.appendChild(controls);
+    //nav.appendChild(projectTitle);
     nav.appendChild(projects);
     nav.appendChild(addProjectUL)
+
+
+    function createProject(title){
+        // const selectProject = document.querySelector(`#${title}Project`);
+        
+        // project 1
+        const project = document.createElement('li');
+        project.id = projectCounter;
+        const projectDiv = document.createElement('div');
+        //projectDiv.id = projectCounter;
+        const projectIconDiv = document.createElement('div');
+        projectIconDiv.id = 'projectIconDiv';
+        const projectIcon = document.createElement('i');
+        projectIcon.classList.add('fas', 'fa-tasks');
+        projectIcon.id = 'projectIcon';
+        const projectTitle = document.createElement('div');
+        projectTitle.id = 'projectTitle';
+        projectTitle.innerText = title;
+        const projectDeleteDiv = document.createElement('div');
+        projectDeleteDiv.id = 'projectDeleteDiv';
+        const projectDelete = document.createElement('i');
+        projectDelete.classList.add('far', 'fa-times-circle');
+        projectDelete.id = 'projectDelete';
+    
+        project.appendChild(projectIconDiv);
+        projectIconDiv.appendChild(projectIcon);
+        project.appendChild(projectTitle);
+        project.appendChild(projectDeleteDiv);
+        projectDeleteDiv.appendChild(projectDelete);
+    
+        // projectTitle.addEventListener('click', () => alert(title));
+        projectTitle.addEventListener('click', () => alert(project.id));
+    
+        projectDeleteDiv.addEventListener('click', () => alert(`delete "${title}" project!`));
+        // projectDeleteDiv.addEventListener('click', () => alert(document.querySelector(`#${title}Project`)));
+    
+        projectCounter++;
+        return project;
+    
+    }
 
     return nav;
 }
@@ -168,7 +183,7 @@ function createInbox(){
     const content = document.createElement('div');
     content.id = 'content';
 
-    //// TODO header
+    //// inbox content header
     const todoTitleUL = document.createElement('ul');
     todoTitleUL.id = 'todoTitleUL';
     const todoTitle = document.createElement('div');
@@ -180,123 +195,20 @@ function createInbox(){
     todoTitleUL.appendChild(todoTitle);
     todoTitleUL.appendChild(todoTitleSort);
 
-    //// main content TODO list items
+    todoTitleSort.addEventListener('click', () => alert('sort the todo list by date'));
+
+    // main content TODO list items
     const todoDisplay = document.createElement('ul'); 
     todoDisplay.id = 'todoDisplay';
+    let todoCounter = 0;
+    todoDisplay.appendChild(createTodo('test todo', 'this is a description, blah blah', '08-05-2021'));
+    todoDisplay.appendChild(createTodo('todo2', 'blah blah', '08-08-2021'));
+    todoDisplay.appendChild(createTodo('laundry', 'washing and folding', '08-11-2021'));
+    todoDisplay.appendChild(createTodo('feed cats', 'very important!!', 'every day'));
 
-    // todo1
-    //checkbox btn
-    const todo1 = document.createElement('li');
-    todo1.classList.add('userTask');
-    const todo1CheckboxDiv = document.createElement('div');
-    todo1CheckboxDiv.id = 'todo1CheckboxDiv';
-    const todo1Checkbox = document.createElement('i');
-    todo1Checkbox.classList.add('far', 'fa-square');
-    todo1Checkbox.id = 'todo1Checkbox';
-    //edit btn
-    const todo1EditDiv = document.createElement('div');
-    todo1EditDiv.id = 'todo1EditDiv';
-    const todo1Edit = document.createElement('i');
-    todo1Edit.classList.add('far', 'fa-edit');
-    todo1Edit.id = 'todo1Edit';
-    //delete btn
-    const todo1DeleteDiv = document.createElement('div');
-    todo1DeleteDiv.id = 'todo1DeleteDiv';
-    const todo1Delete = document.createElement('i');
-    todo1Delete.classList.add('far', 'fa-trash-alt');
-    todo1Delete.id = 'todo1Delete';
-    //description
-    const todoDescription = document.createElement('div');
-    todoDescription.innerText = 'todo1';
-    todoDescription.classList.add('todoDecription');
-    //due date
-    const todoDueDate = document.createElement('div');
-    todoDueDate.innerText = '08-10-2021';
-    todoDueDate.classList.add('todoDueDate');
-    todo1CheckboxDiv.appendChild(todo1Checkbox);
-    todo1.appendChild(todo1CheckboxDiv);
-    todo1.appendChild(todoDescription);
-    todo1EditDiv.appendChild(todo1Edit);
-    todo1.appendChild(todo1EditDiv);
-    todo1DeleteDiv.appendChild(todo1Delete);
-    todo1.appendChild(todo1DeleteDiv);
-    todo1.appendChild(todoDueDate);
-
-    todoDescription.addEventListener('click', () => alert('todo 1 task details'));
-    todo1CheckboxDiv.addEventListener('click', () => alert('cross out the todo'));
-    todo1EditDiv.addEventListener('click', () => alert('edit details of todo'));
-    todo1DeleteDiv.addEventListener('click', () => alert('delete this todo'));
-
-    //todo1Checkbox.onclick = function (){alert('cross out this todo')};
-
-    // todo2
-    const todo2 = document.createElement('li');
-    todo2.classList.add('userTask');
-    const todo2Icon = document.createElement('i');
-    todo2Icon.classList.add('far', 'fa-check-square');
-    todo2Icon.id = 'todo2Icon';
-    //edit and delete btns
-    const todo2Edit = document.createElement('i');
-    todo2Edit.classList.add('far', 'fa-edit');
-    todo2Edit.id = 'todo1Edit';
-    const todo2Delete = document.createElement('i');
-    todo2Delete.classList.add('far', 'fa-trash-alt');
-    todo2Delete.id = 'todo2Delete';
-    const todoDescription2 = document.createElement('div');
-    todoDescription2.innerText = 'todo2';
-    todoDescription2.classList.add('todoDecription');
-    const todoDueDate2 = document.createElement('div');
-    todoDueDate2.innerText = '08-12-2021';
-    todoDueDate2.classList.add('todoDueDate');
-    todo2.appendChild(todo2Icon);
-    todo2.appendChild(todoDescription2);
-    todo2.appendChild(todo2Edit);
-    todo2.appendChild(todo2Delete);
-    todo2.appendChild(todoDueDate2);
-
-    // todo3
-    const todo3 = document.createElement('li');
-    todo3.classList.add('userTask');
-    const todo3Icon = document.createElement('i');
-    todo3Icon.classList.add('far', 'fa-square');
-    todo3Icon.id = 'todo3Icon';
-    //edit and delete btns
-    const todo3Edit = document.createElement('i');
-    todo3Edit.classList.add('far', 'fa-edit');
-    todo3Edit.id = 'todo1Edit';
-    const todo3Delete = document.createElement('i');
-    todo3Delete.classList.add('far', 'fa-trash-alt');
-    todo3Delete.id = 'todo3Delete';
-    const todoDescription3 = document.createElement('div');
-    todoDescription3.innerText = 'todo3';
-    todoDescription3.classList.add('todoDecription');
-    const todoDueDate3 = document.createElement('div');
-    todoDueDate3.innerText = '08-17-2021';
-    todoDueDate3.classList.add('todoDueDate');
-    todo3.appendChild(todo3Icon);
-    todo3.appendChild(todoDescription3);
-    todo3.appendChild(todo3Edit);
-    todo3.appendChild(todo3Delete);
-    todo3.appendChild(todoDueDate3);
-
-
-
-    // example todos
-    const todo4 = document.createElement('li');
-    todo4.innerText = 'todo4';
-    const todo5 = document.createElement('li');
-    todo5.innerText = 'todo5';
-    const todo6 = document.createElement('li');
-    todo6.innerText = 'todo6';
-
-    todoDisplay.appendChild(todo1);
-    todoDisplay.appendChild(todo2);
-    todoDisplay.appendChild(todo3);
-    todoDisplay.appendChild(todo4);
-    todoDisplay.appendChild(todo5);
-    todoDisplay.appendChild(todo6);
-
-    // Add new TODO task option
+    // Add task btn
+    const addLogoDiv = document.createElement('div');
+    addLogoDiv.id = 'addLogoDiv';
     const addLogo = document.createElement('i');
     addLogo.classList.add('fas', 'fa-plus', 'fa');
     addLogo.id = 'addLogo';
@@ -306,15 +218,70 @@ function createInbox(){
     addTask.id = 'addTask';
     const addTaskDescription = document.createElement('div'); 
     addTaskDescription.innerText = 'Add Task';
-
-    addTask.appendChild(addLogo);
+    addLogoDiv.appendChild(addLogo);
+    addTask.appendChild(addLogoDiv);
     addTask.appendChild(addTaskDescription);
     addTaskUL.appendChild(addTask);
+
 
     content.appendChild(todoTitleUL)
     content.appendChild(todoDisplay);
     article.appendChild(content);
     content.appendChild(addTaskUL);
+
+
+
+
+    function createTodo(title, description, dueDate){
+        //checkbox btn
+        const todo = document.createElement('li');
+        todo.classList.add('userTask');
+        todo.id = todoCounter;
+        todoCounter++;
+        const todoCheckboxDiv = document.createElement('div');
+        todoCheckboxDiv.id = 'todoCheckboxDiv';
+        const todoCheckbox = document.createElement('i');
+        todoCheckbox.classList.add('far', 'fa-square');
+        todoCheckbox.id = 'todoCheckbox';
+        //edit btn
+        const todoEditDiv = document.createElement('div');
+        todoEditDiv.id = 'todoEditDiv';
+        const todoEdit = document.createElement('i');
+        todoEdit.classList.add('far', 'fa-edit');
+        todoEdit.id = 'todoEdit';
+        //delete btn
+        const todoDeleteDiv = document.createElement('div');
+        todoDeleteDiv.id = 'todoDeleteDiv';
+        const todoDelete = document.createElement('i');
+        todoDelete.classList.add('far', 'fa-trash-alt');
+        todoDelete.id = 'todoDelete';
+        //description
+        const todoDescription = document.createElement('div');
+        todoDescription.innerText = title;
+        todoDescription.classList.add('todoDecription');
+        //due date
+        const todoDueDate = document.createElement('div');
+        todoDueDate.innerText = dueDate;
+        todoDueDate.classList.add('todoDueDate');
+    
+        todoCheckboxDiv.appendChild(todoCheckbox);
+        todo.appendChild(todoCheckboxDiv);
+        todo.appendChild(todoDescription);
+        todoEditDiv.appendChild(todoEdit);
+        todo.appendChild(todoEditDiv);
+        todoDeleteDiv.appendChild(todoDelete);
+        todo.appendChild(todoDeleteDiv);
+        todo.appendChild(todoDueDate);
+    
+        todoDescription.addEventListener('click', () => alert(description));
+        todoCheckboxDiv.addEventListener('click', () => alert('cross out the todo'));
+        todoEditDiv.addEventListener('click', () => alert('edit details of todo'));
+        todoDeleteDiv.addEventListener('click', () => alert('delete this todo'));
+    
+        return todo;
+    }
+
+
 
     return article;
 }
@@ -325,16 +292,5 @@ export function loadArticle(){
 
 }
 
-// function addEventListeners(){
 
-//     const inbox = document.querySelector('#inbox');
-//     inbox.addEventListener('click', () => alert('inbox'));
-
-//     const today = document.querySelector('#today');
-//     today.addEventListener('click', () => alert('today'));
-
-//     const thisWeek = document.querySelector('#thisWeek');
-//     thisWeek.addEventListener('click', () => alert('thisWeek'));
-
-// }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-import _, { head } from 'lodash';
 import './style.css';
-import Icon from './images/notepad.png'
-import printMe from './print.js';
 import {loadHeader, loadFooter} from './header-footer';
 import {loadMain} from './UI';
 
