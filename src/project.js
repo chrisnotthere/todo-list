d66888d0ed4dc327f796211ed1cf1584f0c31c68
Chris <mr.chrisnash@gmail.com>
2021-08-10 13:23:55 -0700

    added functions to get todos for today and this week

--- src/project.js
+++ src/project.js
@@ -1,5 +1,6 @@
 import './projectList';
 import {allProjectsList} from './UI'
+import { toDate, isToday, isThisWeek, subDays } from 'date-fns'
 
 export class Project{
 
@@ -32,34 +33,51 @@ export class Project{
         this.todos = this.todos.filter((todo) => todo.name != oldTodo);
     }
 
-}
-
-/////////////////////// use similar setup for today and this week
-export class Today {
-
-    constructor(){
-        this.projects = allProjectsList.getProjects();
-        this.todos = allProjectsList.projects.todos;
-    }
-
-    setName(name){
-        this.name = name;
-    }
-
-    getName(name){
-        return this.name;
-    }
-
-    // setTodos(todos){
-    //     this.todos = todos;
-    // }
-
-    // getTodos(){
-    //     return this.todos;
-    // }
-
-    updateInbox(){
-        this.projects = allProjectsList.getProjects();
-    }
+    getTodosToday() {
+        return this.todos.filter((todo) => {
+          const todoDate = new Date(todo.getDateFormatted());
+          return isToday(toDate(todoDate));
+        });
+      }
+    
+      getTodosThisWeek() {
+        return this.todos.filter((todo) => {
+          const todoDate = new Date(todo.getDateFormatted());
+          return isThisWeek(subDays(toDate(todoDate), 1));
+        });
+      }
 
 }
+
+
+
+
+// /////////////////////// use similar setup for today and this week
+// export class Today {
+
+//     constructor(){
+//         this.projects = allProjectsList.getProjects();
+//         this.todos = allProjectsList.projects.todos;
+//     }
+
+//     setName(name){
+//         this.name = name;
+//     }
+
+//     getName(name){
+//         return this.name;
+//     }
+
+//     // setTodos(todos){
+//     //     this.todos = todos;
+//     // }
+
+//     // getTodos(){
+//     //     return this.todos;
+//     // }
+
+//     updateInbox(){
+//         this.projects = allProjectsList.getProjects();
+//     }
+
+// }
